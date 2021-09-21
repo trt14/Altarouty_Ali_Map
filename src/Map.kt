efe508afb3edc95d44a817b8e8fcff8cc768a18e
trt14 <tarooti14dev@gmail.com>
2021-09-21 10:47:14 +0300

    add

--- src/Map.kt
+++ src/Map.kt
@@ -3,10 +3,10 @@ fun main(){
     val weekdays = mutableMapOf(1 to "Sunday", 2 to "Monday",3 to "Tuesday",4 to "Wednesday",5 to "Thursday",6 to "Friday",7 to "Saturday")
 
     for(i in weekdays){
-        println(i)
+        println(i) // print with ID
     }
     for(i in 1..7){
-        println(weekdays[i])
+        println(weekdays[i]) // Print without ID
     }
 
 
